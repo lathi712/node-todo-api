16f4e27795f3996f351c8e4f43c11ae79a0c1bb0
Lathesh Karkera S <karkeralathesh@gmail.com>
2017-04-12 01:09:38 +0400

    Add Patch /todos/:id

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,6 +3,6 @@ const mlabUrl = "mongodb://root:qwerty@ds159220.mlab.com:59220/todoappnode";
 const url = "mongodb://localhost:27017/TodoApp";
 
 mongoose.Promise = global.Promise;
-mongoose.connect(url || mlabUrl);
+mongoose.connect(mlabUrl || url);
 
 module.exports = {mongoose};
